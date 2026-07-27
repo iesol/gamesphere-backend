93acb24cc68258d41f7a61f927f6096e9e6b6c51
Prashanth Molakala <prashanth.molakala@gmail.com>
2026-07-26 20:33:08 -0500

    fix org header in cors

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     origin: origins,
     credentials: true,
     methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-org-id'],
   });
 
   app.use(helmet());
